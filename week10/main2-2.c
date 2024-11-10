6c47bd7976a0686ccc6a692f9e528a3c646fc916
2416916 <a99224719a@gmail.com>
2024-11-10 16:52:14 +0900

    week10

---
+++ week10/main2-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 5
+int main(void) 
+{
+	int i, average;
+	int sum;
+	int grade[SIZE];
+	
+	sum=0;
+	for(i=0;i<SIZE;i++)
+	{
+		printf("Enter the score: ");
+		scanf("%d", &grade[i]);
+		sum+=grade[i];
+	}
+	average=sum/SIZE;
+	printf("Average score: %d\n", average);
+	return 0;
+}
+
